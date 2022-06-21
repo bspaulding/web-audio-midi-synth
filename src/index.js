6a2ab0525b11014d88ad09797e431abeba5131cc
Bradley J. Spaulding <brad.spaulding@gmail.com>
2022-06-21 09:23:28 -0700

    removed on/off button

--- src/index.js
+++ src/index.js
@@ -4,61 +4,37 @@ import makeSynth from "./synth.js";
 import MIDIKeyboard from "./MIDIKeyboard";
 
 function App({ initAudio, synth }) {
-  const [initialized, setInitialized] = useState(false);
   const [octave, setOctave] = useState(2);
   const [chords, setChords] = useState(false);
 
   return (
     <div>
       <h1>MIDI Synth</h1>
-      {!initialized ? (
-        <button
-          onClick={() => {
-            setInitialized(true);
-            synth.start();
-          }}
-        >
-          On
-        </button>
-      ) : (
-        <button
-          onClick={() => {
-            setInitialized(false);
-            synth.stop();
-          }}
-        >
-          Off
-        </button>
-      )}
-      {initialized && (
-        <>
-          <label htmlFor="octave">
-            Octave:{" "}
-            <input
-              type="range"
-              min="-2"
-              max="8"
-              name="octave"
-              value={octave}
-              onInput={(ev) => setOctave(parseInt(ev.target.value, 10))}
-            />
-          </label>
-          <label htmlFor="chords">
-            Chords?{" "}
-            <input
-              type="checkbox"
-              name="chords"
-              value={chords}
-              onChange={(ev) => setChords(ev.target.checked)}
-            />
-          </label>
-          <MIDIKeyboard
-            chords={chords}
-            octave={octave}
-            onEvent={synth.handleMIDI}
-          />
-        </>
-      )}
+      <label htmlFor="octave">
+        Octave:{" "}
+        <input
+          type="range"
+          min="-2"
+          max="8"
+          name="octave"
+          value={octave}
+          onInput={(ev) => setOctave(parseInt(ev.target.value, 10))}
+        />
+      </label>
+      <label htmlFor="chords">
+        Chords?{" "}
+        <input
+          type="checkbox"
+          name="chords"
+          value={chords}
+          onChange={(ev) => setChords(ev.target.checked)}
+        />
+      </label>
+      <MIDIKeyboard
+        chords={chords}
+        octave={octave}
+        onEvent={synth.handleMIDI}
+      />
     </div>
   );
 }
